2e62fec0e2bd3d9c24a78a93029dbfeaa041884b
cit <cheng_it@163.com>
2018-10-25 21:30:01 +0800

    初始化

---
+++ src/test/java/com/cit/designpatterns/AppTest.java
@@ -0,0 +1,13 @@
+package com.cit.designpatterns;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    @Test
+    public void testMain() {
+        System.out.println("Hello World! @Test");
+    }
+}
